5b2f18433eece39b2ba74a9cb2ad3f5d90a1a96a
SkylineBin <xyliubin@outlook.com>
2019-01-03 16:14:31 +0800

    添加 abbr 提取和显示的页面

---
+++ JavaScriptDOM/assets/js/displayAbbreviations.js
@@ -0,0 +1,54 @@
+/*****
+ * 
+ * @function 实现提取 <attr></attr> 标签里面的 title 并以列表的形式打印出来
+ * 
+ */
+
+displayAbbreviations = () => {
+  if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+  // get all abbr elements
+  let abbreviations = document.getElementsByTagName("abbr");
+  if (abbreviations.length < 1) return false;
+  let defs = new Array();
+  // save all attr elements into Array defs
+  for (let i = 0; i < abbreviations.length; i++) {
+    const currentAbbr = abbreviations[i];
+    if (currentAbbr.childNodes.length < 1) continue;
+    let definition = currentAbbr.getAttribute("title");
+    let key = currentAbbr.lastChild.nodeValue;
+    defs[key] = definition;
+  }
+
+  // create table
+  let datalist = document.createElement("dl");
+  // loop reading defs array
+  for (key in defs) {
+    let thisDefinition = defs[key];
+    // create title dt
+    let datatitle = document.createElement("dt");
+    let datatitleText = document.createTextNode(key);
+    // write key into title
+    datatitle.append(datatitleText);
+    // create definition
+    let datadesc = document.createElement("dd");
+    let datadescText = document.createTextNode(thisDefinition);
+    // write definition into description
+    datadesc.append(datadescText);
+    // add title and description into table
+    datalist.append(datatitle);
+    datalist.append(datadesc);
+  }
+
+  if (datalist.childNodes.length < 1) return false;
+
+  // create a h2 header
+  let thisHeader = document.createElement("h2");
+  let thisHeaderText = document.createTextNode("Abbreviations");
+  thisHeader.append(thisHeaderText);
+
+  // add header and datalist into this document
+  document.body.append(thisHeader);
+  document.body.append(datalist);
+}
+
+addLoadEvent(displayAbbreviations);
