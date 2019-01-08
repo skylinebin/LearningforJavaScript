ffe02826a14bd8e7a0baedb62a9f7171580aaafa
SkylineBin <xyliubin@outlook.com>
2019-01-08 10:03:29 +0800

    动画完结版

--- JavaScriptDOM/assets/js/moveElement.js
+++ JavaScriptDOM/assets/js/moveElement.js
@@ -56,4 +56,44 @@ positionMessage = () => {
   moveElement("message", 125, 25, 20);
 }
 
+insertAfter = (newElement, targetElement) => {
+  let parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
+}
+
+
+
+prepareSlideshow = () => {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("linklist")) return false;
+  let slideshow = document.createElement("div");
+  slideshow.setAttribute("id", "slideshow");
+  let preview = document.createElement("img");
+  preview.setAttribute("src", "./assets/images/placeholder.png");
+  preview.setAttribute("alt", "building blocks of web design");
+  preview.setAttribute("id", "preview");
+  slideshow.appendChild(preview);
+
+  let list = document.getElementById("linklist");
+  insertAfter(slideshow, list);
+  let links = document.getElementsByTagName("a");
+
+  links[0].onmouseover = function () {
+    moveElement("preview", -100, 0, 10);
+  }
+  links[1].onmouseover = function () {
+    moveElement("preview", -200, 0, 10);
+  }
+  links[2].onmouseover = function () {
+    moveElement("preview", -300, 0, 10);
+  }
+
+}
+
 addLoadEvent(positionMessage);
+addLoadEvent(prepareSlideshow);
