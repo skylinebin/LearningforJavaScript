33477b880310f85216e6a0c1d1555063815293b0
SkylineBin <xyliubin@outlook.com>
2019-03-01 21:28:54 +0800

    compare function with class

--- ES6/rollupPro/src/index.js
+++ ES6/rollupPro/src/index.js
@@ -1,6 +1,32 @@
-import util1 from './util1.js'
-import { fn1, fn2 } from './util2.js'
+// import util1 from './util1.js'
+// import { fn1, fn2 } from './util2.js'
 
-console.log(util1);
-fn1()
-fn2()
+// console.log(util1);
+// fn1()
+// fn2()
+
+class MathHandle {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  add () {
+    return this.x + this.y;
+  }
+}
+
+const m = new MathHandle(1, 2);
+
+console.log(typeof MathHandle); // 'function'
+// console.log(MathHandle.prototype) // 函数的(显式)原型
+console.log(MathHandle.prototype.constructor === MathHandle) // true 原型的 constructor 属性 等于 构造函数 MathHandle 本身
+console.log(m.__proto__ === MathHandle.prototype) //true m 是 new 出来的实例 实例的隐式原型 == 构造函数的显式原型
+
+// function
+// 可见 class 本身就是 语法糖 就是 'function'
+
+// true
+// 'class' 也有一个显式原型，显式原型的 constructor 属性 也是 'class' 本身
+
+// true
+// m 实例也有隐式原型 等于 'class' 的显式原型
