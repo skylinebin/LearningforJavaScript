e2c2b30954a9fdfb945b85b214d567543f40d0bb
SkylineBin <xyliubin@outlook.com>
2019-03-08 14:27:17 +0800

    create prototype like zepto

---
+++ JavaScriptBasis/prototype/zepto/myZepto.js
@@ -0,0 +1,40 @@
+(function (window) {
+  var zepto = {};
+
+  function Z(dom, selector) {
+    var i, len = dom ? dom.length : 0;
+    for (i=0;i<len;i++){
+      this[i] = dom[i];
+    }
+    this.length = len;
+    this.selector = selector || '';
+  }
+
+  zepto.Z = function (dom, selector) {
+    return new Z(dom, selector);
+  }
+
+  zepto.init = function (selector) {
+    var slice = Array.prototype.slice;
+    var dom = slice.call(document.querySelectorAll(selector));
+    return zepto.Z(dom, selector);
+  }
+
+  var $ = function (selector) {
+    return zepto.init(selector);
+  }
+
+  window.$ = $;
+
+  $.fn = {
+    css: function (key, value) {
+      console.log('css');
+    },
+    html: function (value) {
+      // console.log('html');
+      return 'This is simulated html function';
+    }
+  }
+  Z.prototype = $.fn;
+
+})(window)
