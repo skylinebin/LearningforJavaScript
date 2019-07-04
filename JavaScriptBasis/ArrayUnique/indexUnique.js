2492cf9f5583c66fa8e4015f99effb30946f62e0
SkylineBin <xyliubin@outlook.com>
2019-07-04 19:27:43 +0800

    数组去重实现

--- JavaScriptBasis/ArrayUnique/indexUnique.js
+++ JavaScriptBasis/ArrayUnique/indexUnique.js
@@ -2,7 +2,7 @@
  * @Author: SkylineBin 
  * @Date: 2019-06-10 10:35:44 
  * @Last Modified by: SkylineBin
- * @Last Modified time: 2019-06-10 16:32:40
+ * @Last Modified time: 2019-06-11 09:19:54
  */
 
 
@@ -43,3 +43,69 @@ function unique(a) {
   }
   return res;
 }
+
+function unique2(array) {
+  var res = [];
+  var sortedArray = array.concat().sort();
+  var seen;
+  for (var i = 0, len = sortedArray.length; i < len; i++) {
+      // 如果是第一个元素或者相邻的元素不相同
+      if (!i || seen !== sortedArray[i]) {
+          res.push(sortedArray[i])
+      }
+      seen = sortedArray[i];
+  }
+  return res;
+}
+
+
+// 使用Object 去重的演化之路  
+
+function uniqueObject1(array) {
+  var obj = {};
+  return array.filter(function(item, index, array){
+      return obj.hasOwnProperty(item) ? false : (obj[item] = true)
+  })
+}
+
+// 为了能区分 1 和 '1', 在存储时需要加上类型判断
+function uniqueObject2(array) {
+  var obj = {};
+  return array.filter(function(item, index, array){
+      return obj.hasOwnProperty(typeof item + item) ? false : (obj[typeof item + item] = true)
+  })
+}
+
+// 为了能区分对象类型，再次加入JSON序列化  
+function uniqueObject3(array) {
+  var obj = {};
+  return array.filter(function(item, index, array){
+      console.log(typeof item + JSON.stringify(item))
+      return obj.hasOwnProperty(typeof item + JSON.stringify(item)) ? false : (obj[typeof item + JSON.stringify(item)] = true)
+  })
+}
+
+// 使用 Set 完成 数组去重,只能去除字符串和数字的重复，不能去除对象的重复  
+function uniqueSet(array) {
+  return Array.from(new Set(array));
+}
+
+function uniqueSet2(array) {
+  return [...new Set(array)];
+}
+
+let uniqueSet3 = (a) => [...new Set(a)];
+
+
+// 使用 Map 完成
+
+function uniqueMap(array){
+  const temp = new Map();
+  return array.filter((a) => !temp.has(a) && temp.set(a, 1))
+}
+
+
+
+let array = [{value: 1}, {value: 1}, {value: 2}];
+// let array = [1, 2, 1, 1, '1'];
+console.log(uniqueMap(array));
